af09b2d0234f86740103bcd5352cb81e1d9a2877
briandevoe <bdevoe@brandeis.edu>
2024-07-31 15:56:32 -0400

    Your commit message here

--- R/testing.R
+++ R/testing.R
@@ -4,3 +4,11 @@
 #' testing git2R function
 #'
 ###############################################################################################
+# detach("package:ddkdevtools", unload=TRUE)
+# remove.packages("ddkdevtools")
+# devtools::install_github("Diversity-Data-Kids/ddkdevtools")
+
+library(ddkdevtools)
+
+# testing git2R function
+
