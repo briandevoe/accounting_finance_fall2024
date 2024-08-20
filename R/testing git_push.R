bc10da1f4b33509e8aa93f292257ba1cc90ce172
briandevoe <bdevoe@brandeis.edu>
2024-08-19 22:04:55 -0400

    bd

--- R/testing git_push.R
+++ R/testing git_push.R
@@ -1,3 +1,9 @@
 # testing git function
 
 asdfasdf
+
+git_token <- Sys.getenv("git_token")
+git_username <- Sys.getenv("git_username")
+commit_message <- "bd"
+repo_path <- getwd()
+git_push(commit_message = commit_message, repo_path = repo_path, git_username = git_username, git_token = git_token)
