a2ad8bad7dab2b12c3824e2fef8e508615b5ebd1
briandevoe <bdevoe@brandeis.edu>
2024-08-29 13:32:02 -0400

    bd

--- R/testing git_push.R
+++ R/testing git_push.R
@@ -7,7 +7,14 @@ git_username <- Sys.getenv("git_username")
 commit_message <- "bd"
 repo_path <- getwd()
 getwd()
-git_push(commit_message = commit_message,
+# git_push(commit_message = commit_message,
+#          repo_path = "C:/Users/bdevoe/Desktop/Fall 2024/Accounting & Finance/accounting_finance_fall2024",
+#          git_username = git_username,
+#          git_token = git_token)
+
+
+
+git_push(commit_message = "bd",
          repo_path = "C:/Users/bdevoe/Desktop/Fall 2024/Accounting & Finance/accounting_finance_fall2024",
-         git_username = git_username,
-         git_token = git_token)
+         git_username = Sys.getenv("git_username"),
+         git_token = Sys.getenv("git_token"))
